c3d66c296b1fb147bb0710537f9c6af892db78b7
Anton Apostolatos <antonaf@stanford.edu>
2015-11-01 01:36:48 -0800

    Added opencv feature extraction and error analysis

--- src/classifier.py
+++ src/classifier.py
@@ -6,21 +6,33 @@ from sklearn.cross_validation import train_test_split
 from sklearn import svm
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import ExtraTreesClassifier
+from os import listdir
+from os.path import isfile, join
 
 from sklearn.metrics import accuracy_score, roc_auc_score
 
-def classify(clf):
-	x, y = preprocessing()
-	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=TEST_SET_RATIO)
+import cv2
+
+def classify(clf, train_size=None):
+	x, y = opencv_preprocessing('../images/')
+	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=TEST_SET_RATIO, random_state=42)
+	print len(x_train)
 	clf.fit(x_train, y_train)
 	y_hat = clf.predict(x_test)
+	accuracy_test = accuracy_score(y_test, y_hat)
 
-	print "Accuracy is: {0}".format(accuracy_score(y_test, y_hat))
-	plot_roc(y_test, y_hat)
+	y_hat_train = clf.predict(x_train)
+	accuracy_train = accuracy_score(y_train, y_hat_train)
+	print "Accuracy is: {0}".format(accuracy_test)
+	y_probs = clf.predict_proba(x_test)
+
+	y_probs_train = clf.predict_proba(x_train)
+	plot_roc(y_train, y_probs_train)
+	return accuracy_test, accuracy_train
 
 
 
-def preprocessing():
+def preprocessing(train_size=None):
 	higgs_data = read_data(HIGGS_FILE_NAME)
 	not_higgs_data = read_data(NOT_HIGGS_FILE_NAME)
 
@@ -29,11 +41,46 @@ def preprocessing():
 
 	all_data = permute_arrays(higgs_data, not_higgs_data)
 
+	if train_size is not None and train_size < len(all_data) and train_size > 0:
+		all_data = all_data[:train_size]
+
 	x, y = zip(*all_data)
 	return x, y
 
+def opencv_preprocessing(directory):
+	image_files = [directory + f for f in listdir(directory) if isfile(join(directory,f))]
+	x = []
+	y = []
+	for image in image_files:
+		img = cv2.imread(image)
+		gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+		kp, des = cv2.SURF(400).detectAndCompute(gray,None)
+		if des is not None:
+			x.append(des[0])
+			if 'not' in image:
+				y.append(0)
+			else:
+				y.append(1)
+
+	return x, y
+
+def error_plotting():
+	all_size = [100, 500, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000]
+	train_size = [elem * (1 - TEST_SET_RATIO) for elem in all_size]
+	errors = [classify(ExtraTreesClassifier(n_estimators=500, verbose=1), size) for size in all_size]
+	test_error, train_error = zip(*errors)
+	plot_error(train_size, test_error, train_error)
+
+
+
+
+
+
 if __name__ == '__main__':
-	#classify(LogisticRegression(verbose=1, max_iter=1000))
+	# error_plotting()
+	# classify(LogisticRegression(verbose=1, max_iter=300))
 	# classify(svm.SVC(verbose=1, kernel='poly', max_iter=100000000))
-	classify(ExtraTreesClassifier(n_estimators=1500, verbose=1))
+	classify(ExtraTreesClassifier(n_estimators=300, verbose=1))
+	# opencv_preprocessing('../images/')
 
--- src/util.py
+++ src/util.py
@@ -6,6 +6,9 @@ import matplotlib as ml
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
 
+import scipy
+
+
 
 
 def visualize_plot(arr):
@@ -16,7 +19,7 @@ def visualize_plot(arr):
 
 
 def array_to_matrix(arr):
-	return [[arr[i * 25 + j] for j in range(NUM_PIXELS)] for i in range(NUM_PIXELS)]
+	return [[arr[i * NUM_PIXELS + j] for j in range(NUM_PIXELS)] for i in range(NUM_PIXELS)]
 
 def array_to_numpy_matrix(arr):
 	return np.matrix(array_to_matrix(arr)).astype(float)
@@ -27,8 +30,9 @@ def read_data(filename):
 def permute_arrays(arr1, arr2):
 	return np.random.permutation(np.concatenate((arr1, arr2)))
 
-def plot_roc(y_test, y_hat):
-	fpr, tpr, threshold = roc_curve(y_test, y_hat)
+def plot_roc(y_test, y_probs):
+	print y_probs
+	fpr, tpr, threshold = roc_curve(y_test, y_probs[:,1])
 	roc_auc = auc(fpr, tpr)
 	plt.figure()
 	plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
@@ -41,7 +45,25 @@ def plot_roc(y_test, y_hat):
 	plt.legend(loc="lower right")
 	plt.show()
 
+def plot_error(x, first_error, second_error=None):
+	plt.figure()
+	plt.plot(x, first_error)
+	if second_error is not None:
+		plt.plot(x, second_error)
+	plt.show()
+
+def convert_matrix_to_jpg(array, jpg_file_name):
+	scipy.misc.toimage(array_to_numpy_matrix(array), cmin=0.0).save('../images/' + jpg_file_name)
+
+def convert_all_to_jpg():
+	higgs_data = read_data(HIGGS_FILE_NAME)
+	for i, higgs_matrix in enumerate(higgs_data):
+		convert_matrix_to_jpg(higgs_matrix, 'higgs_{0}.jpg'.format(i))
+	not_higgs_data = read_data(NOT_HIGGS_FILE_NAME)
+	for i, not_higgs_matrix in enumerate(not_higgs_data):
+		convert_matrix_to_jpg(higgs_matrix, 'not_higgs_{0}.jpg'.format(i))
+
 if __name__ == '__main__':
-	higgs_file_data = read_data(HIGGS_FILE_NAME)
-	visualize_plot(higgs_file_data[0])
-	
+	# higgs_file_data = read_data(NOT_HIGGS_FILE_NAME)
+	# visualize_plot(higgs_file_data[0])
+	convert_all_to_jpg()
